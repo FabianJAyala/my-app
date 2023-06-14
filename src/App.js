3c4d1c4ad829daa763c9ad88f58e054acc5bd9e4
Fabian Ayala <fabianjoseayalavargas@gmail.com>
2023-06-14 12:33:41 -0400

    Semantic Structure and Meta Tags

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <img src="./images/Logo.svg"/>
+            </div>
+            <div>
+                <h2>Doormat Navigation</h2>
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#Menu">Menu</a></li>
+                    <li><a href="#Reservations">Reservations</a></li>
+                    <li><a href="#Order-online">Order Online</a></li>
+                    <li><a href="#Login">Login</a></li>
+                </ul>
+            </div>
+            <div>
+                <h2>Contact</h2>
+                <ul>
+                    <li><a href="#Address">Address</a></li>
+                    <li><a href="#About">Phone Number</a></li>
+                    <li><a href="#Menu">Email</a></li>
+                </ul>
+            </div>
+            <div>
+                <h2>Social Media Links</h2>
+                <ul>
+                    <li><a href="#Facebook">Facebok</a></li>
+                    <li><a href="#Twitter">Twitter</a></li>
+                    <li><a href="#Instagram">Instagram</a></li>
+                </ul>
+            </div>
+        </footer>
+    );
+  };
+  export default Footer;
